75213e2d25c18caeaaf8eb2cdd6faf2e1bd99520
Crescentina Ann Remedio <tinayremedio26@gmail.com>
2024-03-02 10:37:54 +0800

    main

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from './Header'
+import Footer from './Footer'
+import Card from  './Card'
+
+function App() {
+  return(
+    <>
+    
+    <Header/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/> 
+    <Footer/>
+    </>
+  )
+  
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+
+
+function Card(){
+    return(
+        <div className = "card">
+            <img src = "https://www.astronomy.com/wp-content/uploads/sites/2/2023/09/underfullmoon.jpeg"    
+            width="200" 
+            height="200"alt = "Profile Picture"></img>
+            <h2>Moon</h2>
+            <p>secretoo</p>
+        </div>
+    );
+}
+export default Card
